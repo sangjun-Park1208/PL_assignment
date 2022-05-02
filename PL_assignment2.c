def298eb87048b5a49e70449d2ae3841587bfb24
Sangjun Park <sangjun1997@naver.com>
2022-05-02 19:04:35 +0900

    Update PL_assignment2.c

--- PL_assignment2.c
+++ PL_assignment2.c
@@ -247,7 +247,8 @@ int lookup(char ch){
 	    case '-':	addChar();	nextToken = SUB_OP;			break;
 	    case '*':	addChar();	nextToken = MULT_OP;		break;
 		case '/':	addChar();  nextToken = DIV_OP;			break;
-	    default:	addChar();	nextToken = EOF;        	break;
+		case '\n':	addChar();	nextToken = EOF;			break;
+	    default:	printf("Syntax error!!\n");				exit(1);
     }
     return nextToken;
 }
